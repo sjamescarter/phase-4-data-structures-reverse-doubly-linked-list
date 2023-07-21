8143d6ff6b87f7d4ce8190585ea892cccf95c499
sjamescarter <stephenjamescarter@gmail.com>
2023-07-20 21:08:34 -0500

    commit tree a62b7cff63d97c7a7332047770f9c1d1084d097f

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,22 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if head.nil?
+    return head if tail.nil?
+
+    reversed = LinkedList.new
+    reversed.head = Node.new(tail.value, next_node = tail.prev_node)
+    byebug
+    puts head.value
+    puts tail.value
+    puts reversed.head.value
+    # reversed.tail = tail.prev_node
+    # reversed.tail = head
+    # reversed
+  end
+
+  private
+  def reverse_node(node)
+    reversed_node = Node.new(node.value, next_node.node.prev_node, prev_node.node.next_node)
   end
 end
